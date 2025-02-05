b7a58a67bfac3f088ed0723cc713164b12c1c9d9
François Petitit <francois.petitit@gmail.com>
2025-02-05 14:18:32 +0100

    [Welcome] Create welcome page with file upload (#1)

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World ! ")
+import streamlit as st
+from PdfReader.pdfreader import extract_text_from_pdf, extract_text_from_docx, extract_text_from_upload
+
+def main():
+    st.title("Bienvenue sur l'outil d'aide à la vérification d'arrêté' municipal")
+    st.write("Veuillez téléverser votre document au format PDF")
+
+    uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+    if uploaded_file is not None:
+        st.write("Fichier bien envoyé!")
+        if st.button("Validate"):
+            st.write("Validation in progress...")
+            # Add your validation logic here
+            st.write("Validation complete!")
+
+if __name__ == "__main__":
+    main()
