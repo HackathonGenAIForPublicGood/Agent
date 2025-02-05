31ec71dfe161c569977af28e7f687af878a7f5ba
François Petitit <francois.petitit@gmail.com>
2025-02-05 14:12:34 +0100

    add PDF reader helpers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PdfReader.pdfreader import extract_text_from_pdf, extract_text_from_docx, extract_text_from_upload
 
 def main():
     st.title("Bienvenue sur l'outil d'aide à la vérification d'arrêté' municipal")
@@ -7,7 +8,7 @@ def main():
     uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
     if uploaded_file is not None:
-        st.write("File uploaded successfully!")
+        st.write("Fichier bien envoyé!")
         if st.button("Validate"):
             st.write("Validation in progress...")
             # Add your validation logic here
